409578cc39526bbdd063516b83552b1676aa1b2c
Reuben <98450819+reubendrummond@users.noreply.github.com>
2024-06-01 17:23:55 +1000

    feat: setup wifi (#1)
    * setup wifi
    
    * change .env config file if running in CI
    
    * fix: clippy warning

--- build.rs
+++ build.rs
@@ -1,3 +1,20 @@
+use std::{env, path::Path};
+
 fn main() {
     embuild::espidf::sysenv::output();
+
+    let is_ci = env::var("CI").is_ok();
+    let env_file = if is_ci {
+        Path::new(".env.example")
+    } else {
+        Path::new(".env")
+    };
+
+    let config = dotenv_build::Config {
+        filename: std::path::Path::new(env_file),
+        fail_if_missing_dotenv: true,
+        ..Default::default()
+    };
+
+    dotenv_build::output(config).unwrap();
 }

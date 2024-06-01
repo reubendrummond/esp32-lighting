ff6a22fd061eb184781d5a71c21ac90e7f700b3c
Reuben <reubendrummond@gmail.com>
2024-06-01 17:02:35 +1000

    change .env config file if running in CI

--- build.rs
+++ build.rs
@@ -1,5 +1,21 @@
+use std::{env, path::Path};
+
 fn main() {
     embuild::espidf::sysenv::output();
 
-    dotenv_build::output(dotenv_build::Config::default()).unwrap();
+    let is_ci = env::var("CI").is_ok();
+    let env_file = if is_ci {
+        Path::new(".env.example")
+    } else {
+        Path::new(".env")
+    };
+
+    let config = dotenv_build::Config {
+        filename: std::path::Path::new(env_file),
+        recursive_search: false,
+        fail_if_missing_dotenv: true,
+        ..Default::default()
+    };
+
+    dotenv_build::output(config).unwrap();
 }

fdda00c09a6261b4855e6f3e8885e835e9f59b2c
Reuben <reubendrummond@gmail.com>
2024-06-09 16:10:09 +1000

    leds working 💡

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub mod html;
+pub mod led;
+pub mod wifi;
--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,19 @@ use esp_idf_svc::{
     nvs::EspDefaultNvsPartition,
     timer::EspTaskTimerService,
 };
-mod html;
-mod wifi;
-use html::{index, IndexProps};
+
+use esp32_lighting::html::{index, IndexProps};
+// use esp32_lighting::led;
+use esp32_lighting::wifi::init_wifi;
+
 use http::Uri;
 use url::form_urlencoded;
-use wifi::init_wifi;
 
+use smart_leds_trait::{SmartLedsWrite, White};
+use ws2812_esp32_rmt_driver::driver::color::LedPixelColorGrbw32;
+use ws2812_esp32_rmt_driver::{LedPixelEsp32Rmt, RGBW8};
+
+// fn main() -> ! {
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
@@ -94,8 +100,29 @@ fn main() {
         })
         .unwrap();
 
+    // led test tings
+
+    let led_pin = peripherals.pins.gpio26;
+    let channel = peripherals.rmt.channel0;
+    let mut ws2812 = LedPixelEsp32Rmt::<RGBW8, LedPixelColorGrbw32>::new(channel, led_pin).unwrap();
+    const NUM_LEDS: usize = 16 * 16;
+
     loop {
-        log::info!("looping...");
-        sleep(Duration::from_secs(2));
+        let pixels = std::iter::repeat(RGBW8::from((6, 0, 0, White(0)))).take(NUM_LEDS);
+
+        ws2812.write(pixels).unwrap();
+        sleep(Duration::from_millis(1000));
+
+        let pixels = std::iter::repeat(RGBW8::from((0, 6, 0, White(0)))).take(NUM_LEDS);
+        ws2812.write(pixels).unwrap();
+        sleep(Duration::from_millis(1000));
+
+        let pixels = std::iter::repeat(RGBW8::from((0, 0, 6, White(0)))).take(NUM_LEDS);
+        ws2812.write(pixels).unwrap();
+        sleep(Duration::from_millis(1000));
+
+        let pixels = std::iter::repeat(RGBW8::from((0, 0, 0, White(6)))).take(NUM_LEDS);
+        ws2812.write(pixels).unwrap();
+        sleep(Duration::from_millis(1000));
     }
 }

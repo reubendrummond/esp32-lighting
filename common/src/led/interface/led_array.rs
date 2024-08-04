0db3a75345b816898470a16bd5755bda1ba47ebb
Reuben <reubendrummond@gmail.com>
2024-08-04 16:11:32 +1000

    document ordered_iter

--- common/src/led/interface/led_array.rs
+++ common/src/led/interface/led_array.rs
@@ -1,5 +1,6 @@
 use crate::led::pixel::Pixel;
 
 pub trait LedArray {
+    /// Returns an iterator over the pixels in the order they should be displayed according to the hardware.
     fn ordered_iter(&self) -> impl Iterator<Item = &Pixel>;
 }
